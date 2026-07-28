089179ea6c882358cce31133fb8c7d4511cceb69
WYN67 <v.alehnovych.fin@gmail.com>
2026-07-28 22:48:26 +0300

    feat: add async database setup with SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
-app = FastAPI(title="Online Library")
+app = FastAPI(title=settings.PROJECT_NAME)
 
 @app.get("/")
 async def root():
